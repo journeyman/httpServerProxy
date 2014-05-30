da082b020e3c57beb8a4d066e45a5be853ac824d
journeyman <journeymanmw@gmail.com>
2014-05-30 10:21:35 +0300

    initial code and test app

---
+++ HttpServerProxy.App/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using HttpServerProxy.App.Resources;
+
+namespace HttpServerProxy.App
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Run();
+        }
+
+        private async void Run()
+        {
+            await ServerProxy.Start("localhost", 3333);
+            player.Source = new Uri("http://localhost:3333");
+        }
+    }
+}
---
+++ HttpServerProxy.App/ServerProxy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Microsoft.Phone.Logging;
+
+namespace HttpServerProxy.App
+{
+    public static class Ext
+    {
+        public static void Log(this object This, string message, [CallerMemberName] string source = "unknown method")
+        {
+            Debug.WriteLine(source + " >>> " + message);
+        }
+    }
+
+    public class ServerProxy
+    {
+        private ServerProxy()
+        {
+            
+        }
+
+        public static async Task Start(string host, int port)
+        {
+            await new ServerProxy().run(host, port);
+        }
+
+        private async Task run(string host, int port)
+        {
+            var listener = new StreamSocketListener();
+            listener.ConnectionReceived += listener_ConnectionReceived;
+            await listener.BindEndpointAsync(new HostName(host), port.ToString());
+        }
+
+        async void listener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            var sb = new StringBuilder();
+            string requestLine = null;
+            //TODO: exceptions handling
+            using (var reader = new StreamReader(args.Socket.InputStream.AsStreamForRead()))
+            {
+                do
+                {
+                    requestLine = await reader.ReadLineAsync();
+                    sb.AppendLine(requestLine);
+                } while (requestLine != null);
+                this.Log(sb.ToString());
+            }
+        }
+    }
+}
